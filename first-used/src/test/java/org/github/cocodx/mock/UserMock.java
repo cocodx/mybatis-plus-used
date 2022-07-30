91f0f9004f0d3b94cbede3fa681fcfd8210bb817
刘刚 <2cevening@gmail.com>
2022-07-31 02:36:10 +0800

    feature(first-used):测试通用service之批量添加功能

---
+++ first-used/src/test/java/org/github/cocodx/mock/UserMock.java
@@ -0,0 +1,32 @@
+package org.github.cocodx.mock;
+
+import com.apifan.common.random.source.InternetSource;
+import com.apifan.common.random.source.NumberSource;
+import com.apifan.common.random.source.PersonInfoSource;
+import org.github.cocodx.entity.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author amazfit
+ * @date 2022-07-31 上午2:22
+ **/
+public class UserMock {
+
+    public static User mockEntity(){
+        User user = new User();
+        user.setName(PersonInfoSource.getInstance().randomNickName(8));
+        user.setEmail(InternetSource.getInstance().randomEmail(10, "gmail.com"));
+        user.setAge(NumberSource.getInstance().randomInt(1, 150));
+        return user;
+    }
+
+    public static List<User> mockList(int count){
+        ArrayList<User> users = new ArrayList<>(count);
+        for (int i = 0; i < count; i++) {
+            users.add(mockEntity());
+        }
+        return users;
+    }
+}
--- first-used/src/test/java/org/github/cocodx/service/impl/UserServiceImplTest.java
+++ first-used/src/test/java/org/github/cocodx/service/impl/UserServiceImplTest.java
@@ -1,12 +1,20 @@
 package org.github.cocodx.service.impl;
 
+import org.github.cocodx.entity.User;
+import org.github.cocodx.mock.UserMock;
 import org.github.cocodx.service.UserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@Transactional
+@Rollback(value = true)
 @SpringBootTest
 public class UserServiceImplTest {
 
@@ -17,6 +25,13 @@ public class UserServiceImplTest {
     public void testGetCount(){
         long count = userService.count();
         System.out.println(count);
+    }
+
+    @Test
+    public void testBatchInsert(){
+        List<User> users = UserMock.mockList(100);
+        boolean b = userService.saveBatch(users);
+        System.out.println("result:"+b);
     }
 
 }
