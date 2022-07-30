5a497901a35a03167957c1db49ccc65fcefdb3a0
刘刚 <2cevening@gmail.com>
2022-07-31 02:16:49 +0800

    feature(first-used):通过Service接口

---
+++ first-used/src/main/java/org/github/cocodx/service/UserService.java
@@ -0,0 +1,7 @@
+package org.github.cocodx.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import org.github.cocodx.entity.User;
+
+public interface UserService extends IService<User> {
+}
---
+++ first-used/src/main/java/org/github/cocodx/service/impl/UserServiceImpl.java
@@ -0,0 +1,13 @@
+package org.github.cocodx.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.github.cocodx.dao.UserMapper;
+import org.github.cocodx.entity.User;
+import org.github.cocodx.service.UserService;
+
+/**
+ * @author amazfit
+ * @date 2022-07-31 上午2:15
+ **/
+public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements UserService {
+}
