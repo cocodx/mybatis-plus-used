be98b188646d10dac7976eff7a8168e3cf1fd910
刘刚 <2cevening@gmail.com>
2022-07-30 19:05:49 +0800

    feature(first-used):对mybatis-plus的查询进行测试

--- first-used/src/main/java/org/github/cocodx/dao/UserMapper.java
+++ first-used/src/main/java/org/github/cocodx/dao/UserMapper.java
@@ -2,11 +2,15 @@ package org.github.cocodx.dao;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.github.cocodx.entity.User;
+import org.springframework.stereotype.Repository;
 
 /**
  * BaseMapper的T，表示要操作的实体
+ * 在这里设置之后，相当于告诉mybatis-plus，数据库字段跟实体对应的映射建立起来
  * @author amazfit
  * @date 2022-07-30 下午6:50
  **/
+//将类或接口，表示成持久层组件
+@Repository
 public interface UserMapper extends BaseMapper<User> {
 }
--- first-used/src/test/java/org/github/cocodx/MybatisPlusTest.java
+++ first-used/src/test/java/org/github/cocodx/MybatisPlusTest.java
@@ -1,8 +1,12 @@
 package org.github.cocodx;
 
 import org.github.cocodx.dao.UserMapper;
+import org.github.cocodx.entity.User;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 /**
  * @author amazfit
@@ -14,5 +18,11 @@ public class MybatisPlusTest {
     @Autowired
     private UserMapper userMapper;
 
-    
+    @Test
+    public void testSelectList(){
+        //查询一个集和，wrapper条件构造器
+
+        List<User> users = userMapper.selectList(null);
+        users.forEach(System.out::println);
+    }
 }
