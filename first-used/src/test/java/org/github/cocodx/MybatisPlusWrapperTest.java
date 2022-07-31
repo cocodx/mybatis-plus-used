33758b7404b2831915e744f794dbefc86d2a465f
刘刚 <2cevening@gmail.com>
2022-07-31 12:23:22 +0800

    feature(first-used):组装删除条件-逻辑删除->update

--- first-used/src/test/java/org/github/cocodx/MybatisPlusWrapperTest.java
+++ first-used/src/test/java/org/github/cocodx/MybatisPlusWrapperTest.java
@@ -15,7 +15,7 @@ import java.util.List;
  * @author amazfit
  * @date 2022-07-31 上午3:54
  **/
-@Rollback(value = true)
+@Rollback(value = false)
 @Transactional
 @SpringBootTest
 public class MybatisPlusWrapperTest {
@@ -38,9 +38,20 @@ public class MybatisPlusWrapperTest {
         //SELECT uid AS id,user_name AS name,age,email,is_deleted FROM t_user WHERE is_deleted=0 ORDER BY age ASC,uid ASC
         //查询用户信息，按照年龄的降序排序，若年龄相同，则按照id升序排序
         QueryWrapper<User> userQueryWrapper = new QueryWrapper<>();
-        userQueryWrapper.orderByAsc("age")
+        userQueryWrapper.orderByDesc("age")
                 .orderByAsc("uid");
         List<User> users = userMapper.selectList(userQueryWrapper);
         users.forEach(System.out::println);
+    }
+
+    @Test
+    public void delete(){
+        //删除邮箱地址为9lxzing@gmail.com的用户信息,还有为null的
+        //UPDATE t_user SET is_deleted=1 WHERE is_deleted=0 AND (email IS NULL OR email = ?)
+        QueryWrapper<User> objectQueryWrapper = new QueryWrapper<>();
+        objectQueryWrapper.isNull("email").or()
+                .eq("email","9lxzing@gmail.com");
+        int delete = userMapper.delete(objectQueryWrapper);
+        System.out.println("result:"+delete);
     }
 }
