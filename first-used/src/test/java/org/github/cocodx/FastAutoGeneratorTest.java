9fa4579c329c0a0f23f9c1db7d4a7706d97a6916
刘刚 <2cevening@gmail.com>
2022-07-31 19:02:55 +0800

    feature(first-used):代码生成器

---
+++ first-used/src/test/java/org/github/cocodx/FastAutoGeneratorTest.java
@@ -0,0 +1,32 @@
+package org.github.cocodx;
+
+import com.baomidou.mybatisplus.generator.FastAutoGenerator;
+import com.baomidou.mybatisplus.generator.config.OutputFile;
+import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
+
+import java.util.Collections;
+
+/**
+ * @author amazfit
+ * @date 2022-07-31 下午6:52
+ **/
+public class FastAutoGeneratorTest {
+
+    public static void main(String[] args) {
+        FastAutoGenerator.create("jdbc:mysql://localhost:3306/mybatis-plus?characterEncoding=utf-8&useSSL=false&serverTimeZone=GMT%2B8", "root", "password").globalConfig(builder -> {
+                    builder.author("liugang") // 设置作者
+//                            .enableSwagger() // 开启 swagger 模式
+                            .fileOverride() // 覆盖已生成文件
+                            .outputDir("D://"); // 指定输出目录
+                }).packageConfig(builder -> {
+                    builder.parent("org.github.cocodx") // 设置父包名
+                            .moduleName("mybatisplus") // 设置父包模块名
+                            .pathInfo(Collections.singletonMap(OutputFile.mapperXml, "D://")); // 设置mapperXml生成路径
+                }).strategyConfig(builder -> {
+                    builder.addInclude("t_user") // 设置需要生成的表名
+                            .addTablePrefix("t_", "c_"); // 设置过滤表前缀
+                }).templateEngine(new FreemarkerTemplateEngine()) // 使用Freemarker引擎模板，默认的是Velocity引擎模板
+                .execute();
+
+    }
+}
